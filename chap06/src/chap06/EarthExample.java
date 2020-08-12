119f0bfd0535e8bce1bb9fcc04fe203f589996f9
LeeYongMin01 <vbn0213@gmail.com>
2020-08-12 20:46:54 +0900

    8월 12일 자습

---
+++ chap06/src/chap06/EarthExample.java
@@ -0,0 +1,13 @@
+package chap06;
+
+import chap06.util.Earth;
+
+// 상수 사용
+
+public class EarthExample {
+  public static void main(String[] args) {
+    System.out.println("지구의 반지름: " + Earth.EARTH_RADIUS + " km");
+    System.out.println("지구의 표면적: " + Earth.EARTH_SURFACE_AREA + " km^2");
+
+  }
+}
---
+++ chap06/src/chap06/util/Person.java
@@ -0,0 +1,31 @@
+package chap06.util;
+
+// final 필드 선언과 초기화
+
+public class Person {
+
+  public final String nation = "Korea";
+  public final String ssn;
+  public String name;
+
+  public Person(String ssn, String name) {
+    this.ssn = ssn;
+    this.name = name;
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
