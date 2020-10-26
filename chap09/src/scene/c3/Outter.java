c9b6910ccd87813aba04a18a6293c436a2d64383
LeeYongMin01 <vbn0213@gmail.com>
2020-10-27 00:27:37 +0900

    10/27

--- chap09/src/scene/c3/Outter.java
+++ chap09/src/scene/c3/Outter.java
@@ -4,10 +4,12 @@ public class Outter {
 
   public void method(int arg) {
     int localVariable = 1;
-    class Inner {
-      public void method() {
-        int result = arg + localVariable;
+    
+      
+      class Inner {
+        public void method() {
+          int result = arg + localVariable;
+        }
       }
-    }
   }
 }
