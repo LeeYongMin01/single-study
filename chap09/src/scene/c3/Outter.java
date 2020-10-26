0b00662bb146dc83ec847c19a307c708822ee5db
LeeYongMin01 <vbn0213@gmail.com>
2020-10-26 23:58:15 +0900

    10/26

---
+++ chap09/src/scene/c3/Outter.java
@@ -0,0 +1,13 @@
+package scene.c3;
+
+public class Outter {
+
+  public void method(int arg) {
+    int localVariable = 1;
+    class Inner {
+      public void method() {
+        int result = arg + localVariable;
+      }
+    }
+  }
+}
