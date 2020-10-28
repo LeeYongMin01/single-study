2e8ce6d84233e590b4af432265a56b485ed7cbdf
LeeYongMin01 <vbn0213@gmail.com>
2020-10-28 20:27:58 +0900

    10/28

---
+++ chap12/src/scene/c2/ThreadB.java
@@ -0,0 +1,10 @@
+package scene.c2;
+
+public class ThreadB extends Thread {
+  @Override
+  public void run() {
+    for(int i = 0; i < 2; i++) {
+      System.out.println(getName() + "가 출력한 내용");
+    }
+  }
+}
