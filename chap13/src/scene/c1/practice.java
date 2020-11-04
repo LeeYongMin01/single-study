74e68c24f21815f82b906d97dabe245d4e825ced
LeeYongMin01 <vbn0213@gmail.com>
2020-11-04 17:26:09 +0900

    11/4

---
+++ chap13/src/scene/c1/practice.java
@@ -0,0 +1,13 @@
+package scene.c1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class practice {
+  public static void main(String[] args) {
+
+  List list = new ArrayList();
+  list.add("hello");
+  String str = (String) list.get(0);
+  }
+}
